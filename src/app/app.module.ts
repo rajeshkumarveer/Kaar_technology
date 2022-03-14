2af0e9ebdb57f4bdffa031ce31c65e1eefb3bd37
Rajesh kumar <rajeshkumarveer2305@gmail.com>
2022-03-15 02:21:15 +0530

    add all features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { OurServicesComponent } from './components/our-services/our-services.component';
+import { AchievementComponent } from './components/achievement/achievement.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent, 
+    OurServicesComponent, AchievementComponent, TeamsComponent, ContactsComponent, FooterComponent,
   ],
   imports: [
     BrowserModule
